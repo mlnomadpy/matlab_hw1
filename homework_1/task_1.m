7e370e8ca5fc054b75e166e899935e23d1b8c337
Taha Bouhsine <45195299+mlnomadpy@users.noreply.github.com>
2024-09-27 15:40:20 +0000

    we solved the first task, pat on the back for me

---
+++ homework_1/task_1.m
@@ -0,0 +1,17 @@
+% Author Name: Taha Bouhsine
+% Email: tahabhs14@gmail.com
+% Course: MATLAB Programming - Fall 2024
+% Assignment: Homework 1
+
+% 1. Variable Operations (20 points)
+% Creating Variables
+r = 5 % Radius of Circle
+%Calculation of Area of the Circle
+a =... % Areaa of Circle
+% c) Calculate the circumference of the circle and store it in a variable called `circumference`.
+% Calculating the Circumference of the Circle
+c = ... % Circumference of Circle
+% Displaying Results to User
+fprintf("For a Circle of radius %f", r)
+fprintf("The area is %f \n", a)
+fprintf("The Circumference is %f \n", c)
